e5184fa7d57b11c8271eb78f61403587b87cc211
Yoav Assulin <yoav@paytweed.com>
2024-02-04 20:05:55 +0200

    fix: add logs

--- app/tweed.ts
+++ app/tweed.ts
@@ -2,16 +2,22 @@
 import { TweedBackendSDK } from "@paytweed/backend-sdk";
 
 export const tweed = async (message: string) => {
-  const tweed = await TweedBackendSDK.setup({
-    apiKey: process.env.TWEED_API_KEY as string,
-    apiSecret: process.env.TWEED_API_SECRET as string,
-    defaultBlockchainIds: ["ethereumSepolia"],
-  });
+    console.log("send message to tweed", message);
+    console.log("stringify", JSON.stringify({ message }));
+
+    const tweed = await TweedBackendSDK.setup({
+      apiKey: process.env.TWEED_API_KEY as string,
+      apiSecret: process.env.TWEED_API_SECRET as string,
+      defaultBlockchainIds: ["ethereumSepolia"],
+    });
+
   const answer = await tweed.handleMessageFromFrontend(
     JSON.stringify({ message }),
     "1",
     "a@a.com"
   );
+
+  console.log("answer", answer);
 
   return answer;
 };
